8b3d0dce20cf1f79fb8cda7a18b52a7122887eb1
Oscar <Oscar@mbp16.local>
2020-04-04 12:07:13 +0200

    Finished Day1

---
+++ Day1/Constants.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let taylor = "Swift"
+
+// INFORMATION : It's preferable to use constants (let) instead variables (var)!!
+
+
---
+++ Day1/DoublesAndBooleans.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var pi = 3.141
+
+var awesome = true
+
