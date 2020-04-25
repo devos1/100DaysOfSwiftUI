3a96f217080cabcddaba95552d90081986685f73
Oscar <Oscar@mbp16.local>
2020-04-25 16:39:05 +0200

    Finished Day 24

--- Days23-24/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Days23-24/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,108 +8,106 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var useRedText = false
-//    let motto1 = Text("Draco dormiens")
-//    let motto2 = Text("nunquam titillandus")
+struct ContentView: View {
+    @State private var useRedText = false
+    let motto1 = Text("Draco dormiens")
+    let motto2 = Text("nunquam titillandus")
+
+    var body: some View {
+
+        Text("Test Toto...")
+            .largeBlue()
+
+//        Text("Hello Toto")
+//            .titleStyle()
+
+//        Text("Hello World")
+//        .modifier(Title())
+
+        // Avoir to copy the same things
+//        VStack(spacing: 10) {
+//            CapsuleText(text: "First")
+//            CapsuleText(text: "Second")
+//                .foregroundColor(.yellow)
+//        }
+
 //
-//    var body: some View {
+//      VStack(spacing: 10) {
+//          Text("First")
+//              .font(.largeTitle)
+//              .padding()
+//              .foregroundColor(.white)
+//              .background(Color.blue)
+//              .clipShape(Capsule())
 //
-//        Color.blue
-//        .frame(width: 300, height: 200)
-//        .watermarked(with: "Hacking with Swift")
-//
-////        Text("Hello Toto")
-////            .titleStyle()
-//
-////        Text("Hello World")
-////        .modifier(Title())
-//
-//        // Avoir to copy the same things
-////        VStack(spacing: 10) {
-////            CapsuleText(text: "First")
-////            CapsuleText(text: "Second")
-////                .foregroundColor(.yellow)
-////        }
-//
-////
-////      VStack(spacing: 10) {
-////          Text("First")
-////              .font(.largeTitle)
-////              .padding()
-////              .foregroundColor(.white)
-////              .background(Color.blue)
-////              .clipShape(Capsule())
-////
-////          Text("Second")
-////              .font(.largeTitle)
-////              .padding()
-////              .foregroundColor(.white)
-////              .background(Color.blue)
-////              .clipShape(Capsule())
-////      }
-//
-////        VStack{
-////            motto1
-////                .foregroundColor(Color.red)
-////            motto2
-////                .foregroundColor(Color.blue)
-////        }
-//
-//        // In this case the inner modifier doesn't replace but add to VStack
-////        VStack {
-////            Text("Gryffindor")
-////                .blur(radius: 0)
-////            Text("Hufflepuff")
-////            Text("Ravenclaw")
-////            Text("Slytherin")
-////        }
-////        .blur(radius: 5)
-//
-////        VStack {
-////            Text("Gryffindor")
-////                .font(.largeTitle)  // Overrides parent modifier
-////            Text("Hufflepuff")
-////            Text("Ravenclaw")
-////            Text("Slytherin")
-////        }
-////        .font(.title)
-//
-////        Button("Hello World") {
-////            // flip the Boolean between true and false
-////            self.useRedText.toggle()
-////        }
-////        .foregroundColor(useRedText ? .red : .blue)
-//
-////        Text("Hello World")
-////        .padding()
-////        .background(Color.red)
-////        .padding()
-////        .background(Color.blue)
-////        .padding()
-////        .background(Color.green)
-////        .padding()
-////        .background(Color.yellow)
-//
-////        Button("Hello World") {
-////            print(type(of: self.body))
-////        }
-////        .frame(width: 200, height: 200)
-////        .background(Color.red)
-//
-////        Text("Hello, World!")
-////            .frame(maxWidth: .infinity, maxHeight: .infinity)
-////            .background(Color.red)
-////            .edgesIgnoringSafeArea(.all)
-//    }
-//}
+//          Text("Second")
+//              .font(.largeTitle)
+//              .padding()
+//              .foregroundColor(.white)
+//              .background(Color.blue)
+//              .clipShape(Capsule())
+//      }
+
+//        VStack{
+//            motto1
+//                .foregroundColor(Color.red)
+//            motto2
+//                .foregroundColor(Color.blue)
+//        }
+
+        // In this case the inner modifier doesn't replace but add to VStack
+//        VStack {
+//            Text("Gryffindor")
+//                .blur(radius: 0)
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .blur(radius: 5)
+
+//        VStack {
+//            Text("Gryffindor")
+//                .font(.largeTitle)  // Overrides parent modifier
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .font(.title)
+
+//        Button("Hello World") {
+//            // flip the Boolean between true and false
+//            self.useRedText.toggle()
+//        }
+//        .foregroundColor(useRedText ? .red : .blue)
+
+//        Text("Hello World")
+//        .padding()
+//        .background(Color.red)
+//        .padding()
+//        .background(Color.blue)
+//        .padding()
+//        .background(Color.green)
+//        .padding()
+//        .background(Color.yellow)
+
+//        Button("Hello World") {
+//            print(type(of: self.body))
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(Color.red)
+
+//        Text("Hello, World!")
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color.red)
+//            .edgesIgnoringSafeArea(.all)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
 
 struct CapsuleText: View {
     var text: String
@@ -156,48 +154,26 @@ struct Watermark: ViewModifier {
     }
 }
 
+struct LargeBlue : ViewModifier{
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(Color.blue)
+    }
+    
+    
+}
+
+extension View{
+    func largeBlue() -> some View {
+        self.modifier(LargeBlue())
+    }
+}
+
 extension View {
     func watermarked(with text: String) -> some View {
         self.modifier(Watermark(text: text))
     }
-}
 
-
-/*
- Custom Containers - more advanced topic
- */
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
-    }
-
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<self.columns, id: \.self) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct ContentView: View {
-    var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-//            Text("R\(row) C\(col)")
-            HStack {
-                 Image(systemName: "\(row * 4 + col).circle")
-                 Text("R\(row) C\(col)")
-             }
-        }
-    }
 }
