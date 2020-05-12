7f5bb07274f64a492268da6043be5875b8111fd2
Oscar <Oscar@mbp16.local>
2020-05-12 21:44:39 +0200

    Finished Day 40

---
+++ Days39-42/MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,17 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Oscar da Silva on 12.05.20.
+//  Copyright © 2020 Oscar da Silva. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
---
+++ Days39-42/MoonShot/MoonShot/Mission.swift
@@ -0,0 +1,39 @@
+//
+//  Mission.swift
+//  MoonShot
+//
+//  Created by Oscar da Silva on 12.05.20.
+//  Copyright © 2020 Oscar da Silva. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
